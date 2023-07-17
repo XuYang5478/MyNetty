cf3741eec465c5e94086ce34b659da2022c8b52b
xuyang <1826712273@qq.com>
2023-07-17 17:18:06 +0800

    first commit

---
+++ src/main/java/org/example/client/EchoClient.java
@@ -0,0 +1,15 @@
+package org.example.client;
+
+public class EchoClient {
+    private final String host;
+    private final int port;
+
+    public EchoClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void start() {
+
+    }
+}
