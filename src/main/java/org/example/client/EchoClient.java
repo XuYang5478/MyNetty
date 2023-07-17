479734687da221a4523d8994f5d03d8cfe8c6875
xuyang <1826712273@qq.com>
2023-07-17 17:29:58 +0800

    编写 EchoClient

--- src/main/java/org/example/client/EchoClient.java
+++ src/main/java/org/example/client/EchoClient.java
@@ -1,5 +1,16 @@
 package org.example.client;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import org.example.client.handler.EchoClientHandler;
+
+import java.net.InetSocketAddress;
+
 public class EchoClient {
     private final String host;
     private final int port;
@@ -9,7 +20,26 @@ public class EchoClient {
         this.port = port;
     }
 
-    public void start() {
+    public void start() throws InterruptedException {
+        EventLoopGroup group=new NioEventLoopGroup();
+        try{
+            Bootstrap bootstrap=new Bootstrap();
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .remoteAddress(new InetSocketAddress(host,port))
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new EchoClientHandler());
+                        }
+                    });
+            ChannelFuture future=bootstrap.bind().sync();
+            future.channel().closeFuture().sync();
+        }finally {
+            group.shutdownGracefully().sync();
+        }
+    }
+
+    public static void main(String[] args) {
 
     }
 }
